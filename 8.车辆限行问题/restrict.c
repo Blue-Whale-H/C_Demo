54059e0580b1b7b5c55ae05d8eb5fee900285f40
Blue_Whale_H <996586214@qq.com>
2017-11-21 15:51:00 +0800

    车辆限行问题

---
+++ 8.车辆限行问题/restrict.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+
+enum day
+{
+	MONDAY = 1,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+};
+
+char get_last_char(char str[])
+{
+	char c;
+	int i = 0;
+	
+	while (str[i])
+	{
+		c = str[i];
+		i++;
+	}
+
+	return c;
+}
+
+int is_restricted(int tail_num, enum day today)
+{
+	int ret = 0;
+	// taile_num: 0, 1, --- 9
+	// 0, 5 
+	// 1, 6
+	// ...
+	
+	switch (tail_num)
+	{
+		case 0:
+		case 5:
+			ret = (today == MONDAY) ? 1 : 0;
+			break;
+
+		case 1:
+		case 6:
+			ret = (today == TUESDAY) ? 1 : 0;
+			break;
+
+		case 2:
+		case 7:
+			ret = (today == WEDNESDAY) ? 1 : 0;
+			break;
+
+		case 3:
+		case 8:
+			ret = (today == THURSDAY) ? 1 : 0;
+			break;
+
+		case 4:
+		case 9:
+			ret = (today == FRIDAY) ? 1 : 0;
+			break;
+
+		default:
+			ret = 0;
+	}
+
+	return ret;
+}
+
+enum day get_week_day(int year, int month, int day)
+{
+	// 1970-1-1 : week day 4 (THURSDAY)
+	// 2017-1-1 : week day 7 (SUNDAY)
+	int origin_day = 7;  // 2017-1-1 : 7
+	int m[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	int alldays = 0;
+	int i = 0;
+
+	for (i = 1; i < month; i++)
+	{
+		// i = 1 -> m[0];
+		alldays += m[i-1];
+	}
+
+	alldays += day - 1;
+	printf("alldays = %d\n", (alldays + origin_day) % 7);
+
+	switch ((alldays + origin_day) % 7)
+	{
+		case 0:
+			return SUNDAY;
+		case 1:
+			return MONDAY;
+		case 2:
+			return TUESDAY;
+		case 3:
+			return WEDNESDAY;
+		case 4:
+			return THURSDAY;
+		case 5:
+			return FRIDAY;
+		default:
+			return SATURDAY;
+	}
+}
+
+int main(void)
+{
+	char car_num_str[8] = "A12345";
+	int tail_num = 0;   // "A23456" -> 6
+	enum day today;     // 1-7
+	char last_char;
+	int ret;
+
+	printf("Please input your car number:");
+	scanf("%s", car_num_str);
+	
+	printf("car num is %s\n", car_num_str);
+
+	/* get the last char of car number string */
+	last_char = get_last_char(car_num_str);
+	printf("last char is %c\n", last_char);
+	tail_num = last_char - '0';
+	//today = FRIDAY;
+	today = get_week_day(2017, 8, 25);
+
+	ret = is_restricted(tail_num, today);
+	if(ret == 1)
+		printf("restricted!\n");
+	else
+		printf("NOT restricted!\n");
+
+	return 0;
+}
